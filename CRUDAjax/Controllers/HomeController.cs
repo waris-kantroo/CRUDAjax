17f60fb0e8908128a545e1f46426d839e868875f
waris-kantroo <47562096+waris-kantroo@users.noreply.github.com>
2019-03-19 19:36:55 +0530

    Add project files.

---
+++ CRUDAjax/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using CRUDAjax.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDAjax.Controllers
+{
+    public class HomeController : Controller
+    {
+        EmployeeDB empDB = new EmployeeDB();
+        // GET: Home  
+        public ActionResult Index()
+        {
+            var test = empDB.ListAll();
+            return View();
+        }
+        public JsonResult List()
+        {
+            return Json(empDB.ListAll(), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Add(Employee emp)
+        {
+            return Json(empDB.Add(emp), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetbyID(int ID)
+        {
+            var Employee = empDB.ListAll().Find(x => x.EmployeeID.Equals(ID));
+            return Json(Employee, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Update(Employee emp)
+        {
+            return Json(empDB.Update(emp), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Delete(int ID)
+        {
+            return Json(empDB.Delete(ID), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ CRUDAjax/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRUDAjax.Models
+{
+    public class Employee
+    {
+
+        public int EmployeeID { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string State { get; set; }
+
+        public string Country { get; set; }
+    }
+}
